cb34362e355edc6136c50e6a9891beabc2cf727f
Utku Sert <utkusert.arc@gmail.com>
2025-07-18 11:58:27 +0300

    1

--- js/task-1.js
+++
@@ -1,9 +0,0 @@
-const item = document.querySelectorAll('.item');
-const categories = document.querySelector('#categories');
-console.log(`Number of categories: ${categories.children.length}`);
-item.forEach(el => {
-  const title = el.querySelector('h2');
-  const elements = el.querySelectorAll('ul li');
-  console.log(`Category: ${title.textContent}`);
-  console.log(`Elements: ${elements.length}`);
-});
--- js/task-3.js
+++
@@ -1,5 +0,0 @@
-const input = document.querySelector('#name-input');
-const output = document.querySelector('#name-output');
-input.addEventListener('input', event => {
-  output.textContent = event.target.value.trim() || 'Anonymous';
-});
